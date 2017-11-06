ee19ab0398fcef1774da947f234ab5116b9b6d2a
d-king27 <kingd8883@gmail.com>
2017-11-06 00:32:31 +0000

    set up files and dependancies

---
+++ spec/spec.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+var _ = require('../index.js');
+
+describe('_ lowbar',() => {
+    'use strict'
+    it('is an object',  () => {
+        expect(_).to.be.an('object');
+      });
+
+})
