13e6f7f0466132d99ab6e5f538591f590ca1d57b
d-king27 <kingd8883@gmail.com>
2017-11-08 15:04:10 +0000

    completed each, indexof and filter, all fully tested

--- index.js
+++ index.js
@@ -20,7 +20,66 @@ _.last = function (input, n = 1) {
     return input;
 
 };
+_.each = function (list, fn) {
+    if (typeof list === 'string') {return _.each(list.split(''), fn) }
+    if (typeof list !== 'object') { return list; }
+    if (list === null) { return null; }
+    if (Array.isArray(list)) {
+      for (let i = 0; i < list.length; i++) {
+        fn(list[i]);
+      }
+    }
+    else {
+      var values = Object.values(list);
+      for (let j = 0; j < values.length; j++) {
+        fn(values[j]);
+      }
+    }
+  };
 
+  _.indexOf = function (arr, val) {
+    let final = -1;
+    if (!Array.isArray(arr)) return final;
+    if (arr[0] === undefined) return final;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === val) {
+        final = i;
+        return final;
+      }
+    }
+    return final;
+  };
+
+  _.filter = function (list, pred){
+    let final = []
+    let genPushFunc = (fn) =>{
+        return (a) => {
+            if (fn(a) === true){
+                final.push(a)
+            }
+        }
+    }
+    _.each(list,genPushFunc(pred))
+    return final  
+
+  }
+
+  _.reject = function (list, pred){
+    let final = []
+    let genPushFunc = (fn) =>{
+        return (a) => {
+            if (fn(a) === false){
+                final.push(a)
+            }
+        }
+    }
+    _.each(list,genPushFunc(pred))
+    return final  
+
+  }
+  _.uniq = function(list){
+      
+  }
 
 
 module.exports = _
--- spec/spec.js
+++ spec/spec.js
@@ -2,11 +2,6 @@ var expect = require('chai').expect;
 
 var _ = require('../index.js');
 
-let count;
-const incrCount = () => {
-  count++;
-
-}
 
 
 describe('_ lowbar', () => {
@@ -79,7 +74,8 @@ describe('_ lowbar', () => {
             expect(_.last('hello', 2)).to.eql('lo');
             expect(_.last([1, 2, 3, 4], 3)).to.eql([2, 3, 4]);
         });
-    })
+    });
+    
 
     describe('#each',  () => {
         it('is a function', () => {
@@ -89,20 +85,127 @@ describe('_ lowbar', () => {
         it(`if passed with an array as the list arguement, 
         calls the function the amount of times equal to the length`, () => {
             let count = 0;
+            let incrCount = () => {
+                count++;
+               
+               }           
+          
             _.each([1, 2, 3], incrCount);
             expect(count).to.equal(3);
           });
         it(`if passed with an object, 
           calls the function the amount of times equal to the amount of key value pairs`,() => {
             let count = 0;
+            let incrCount = () => {
+                count++;
+               
+               }  
             _.each({ a: 1, b: 2, c: 3 }, incrCount);
             expect(count).to.equal(3);
           });
         it(`if passed with a string, 
         calls the function the amount of times equal to the length`,  () => {
             let count = 0;
+            let incrCount = () => {
+                count++;
+               
+               }  
             _.each('wow', incrCount)
             expect(count).to.equal(3);
           });
+      });
+
+      describe('#indexOf',  () => {
+        it('is a function', () => {
+          expect(_.indexOf).to.be.a('function');
+        });
+        it('it returns -1 if value is not found or undefined', () => {
+          expect(_.indexOf()).to.equal(-1);
+          expect(_.indexOf([])).to.equal(-1);
+          expect(_.indexOf({})).to.equal(-1);
+          expect(_.indexOf([1, 2, 3], 4)).to.equal(-1);
+        });
+        it('returns the index of search value', () => {
+          expect(_.indexOf([1, 2, 3], 1)).to.equal(0);
+          expect(_.indexOf([1, 2, 3], 2)).to.equal(1);
+          expect(_.indexOf(['hello', 'goodbye'], 'hello')).to.equal(0);
+        });
+      });
+      describe('#filter',  () => {
+        it('it is a function',  () => {
+          expect(_.filter).to.be.a('function');
+        });
+        it('returns an empty array when passed with an empty array', () => {
+          expect(_.filter([])).to.eql([]);
+          expect(_.filter([], 5)).to.eql([]);
+          expect(_.filter()).to.eql([]);
+    
+        });
+        it('returns an array filtered with the predicate when passed with an array',  () => {
+         let lowerThan10 = (a) => {
+            return a < 10;
+          }
+          expect(_.filter([1, 2, 11, 12, 10, 9], lowerThan10)).to.eql([1, 2, 9]);
+          expect(_.filter([7, 6, 5, 100, 47, 2], lowerThan10)).to.eql([7, 6, 5, 2]);
+        });
+        it('returns an array filtered with the predicate when passed with a string',  () => {
+            let letterIsVowel = (a) => {
+                let l = a.toLowerCase()
+               return l === 'a'|| l === 'e' || l === 'i'|| l === 'o'|| l === 'u' ;
+             }
+             expect(_.filter('hello', letterIsVowel)).to.eql(['e','o']);
+           });
+        it('returns an array filtered with the predicate when passed with an object',  () => {
+            let greaterThan10 = (a) => {
+                return a > 10;
+              }
+          expect(_.filter({ a: 1, b: 20, c: 9 }, greaterThan10)).to.eql([20]);
+          expect(_.filter({ a: 5, b: 10, c: 15, d: 90}, greaterThan10)).to.eql([15,90]);
+        });
+    });
+    describe('#reject',  () => {
+        it('it is a function',  () => {
+          expect(_.filter).to.be.a('function');
+        });
+        it('returns an empty array when passed with an empty array', () => {
+          expect(_.filter([])).to.eql([]);
+          expect(_.filter([], 5)).to.eql([]);
+          expect(_.filter()).to.eql([]);
+    
+        });
+        it('returns an array filtered with the predicate when passed with an array',  () => {
+         let lowerThan10 = (a) => {
+            return a < 10;
+          }
+          expect(_.reject([1, 2, 11, 12, 10, 9], lowerThan10)).to.eql([11, 12,10]);
+          expect(_.reject([7, 6, 5, 100, 47, 2], lowerThan10)).to.eql([100,47]);
+        });
+        it('returns an array filtered with the predicate when passed with a string',  () => {
+            let letterIsVowel = (a) => {
+                let l = a.toLowerCase()
+               return l === 'a'|| l === 'e' || l === 'i'|| l === 'o'|| l === 'u' ;
+             }
+             expect(_.reject('hello', letterIsVowel)).to.eql(['h','l','l']);
+           });
+        it('returns an array filtered with the predicate when passed with an object',  () => {
+            let greaterThan10 = (a) => {
+                return a > 10;
+              }
+          expect(_.reject({ a: 1, b: 20, c: 9 }, greaterThan10)).to.eql([1,9]);
+          expect(_.reject({ a: 5, b: 10, c: 15, d: 90}, greaterThan10)).to.eql([5,10]);
+        });
+    });
+    describe('#uniq', function () {
+        it('it is a function',  () => {
+          expect(_.uniq).to.be.a('function');
+        });
+        it('returns the input array if all members are not unique',  () => {
+          expect(_.uniq([1, 2, 3])).to.eql([1, 2, 3]);
+          expect(_.uniq([])).to.eql([]);
+    
+        });
+        it('removes repeated items from an array', () => {
+          expect(_.uniq([1, 1, 2])).to.eql([1, 2]);
+        });
       });
 })
